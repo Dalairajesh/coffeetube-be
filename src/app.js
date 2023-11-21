803d99f7a57094014c17ee6d67a9d3217b1264f5
Dalairajesh <dalairajesh22@gmail.com>
2023-11-21 21:03:26 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { express } from "express";
+import  express  from "express";
 import cors from "cors"
 import cookieParser from "cookie-parser";
 
@@ -15,7 +15,15 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes import
 
+import userRouter from './routes/user.routes.js'
+
+
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
 
 
 export { app }
